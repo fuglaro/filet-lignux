f33f1005723ed9729206c1b68098361c806a4631
John K van Leeuwen <jvl@convex.cc>
2021-02-11 11:45:27 +0000

    Added dwm as a fork. The suckless team is glorious.

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 # filet-lignux
 A developer's linux configuration - a palatable lean slice.
 
-# Thanks to
+# Thanks to, grateful forks, and contributions
+
+We stand on the shoulders of giants. They own this, far more than I do.
+
+https://github.com/torvalds/linux
 https://ethanschoonover.com/solarized/
+https://dwm.suckless.org/
--- install.sh
+++ install.sh
@@ -1,10 +1,19 @@
-#!/bin/bash
+#!/bin/bash -e
 
+# Install base code.
 mkdir -p ~/.config
 rsync -a filetlignux/ ~/.config/filetlignux
 
+# Install Xorg color scheme.
 touch ~/.xprofile
 awk -vl=\
 'xrdb -merge ~/.config/filetlignux/xorg/solarized.Xresources'\
     '$0=l {o==1} END {if(!o) print l} 1' ~/.xprofile > ~/.xprofile
+
+# Build dwm (forked) and configure.
+cd ~/.config/filetlignux/dwm/
+make clean
+make
+cd -
+
 
