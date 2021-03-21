79c3de39108dcd25176a93865637b93d92180f8c
John K van Leeuwen <jvl@convex.cc>
2021-03-21 13:43:18 +0000

    Trimmed the border to 1px

--- filetlignux/dwm/config.def.h
+++ filetlignux/dwm/config.def.h
@@ -26,7 +26,7 @@
 #define BACKGREY "#111111"
 
 /* appearance */
-static const unsigned int borderpx  = 2; /* border pixel of windows */
+static const unsigned int borderpx  = 1; /* border pixel of windows */
 static const unsigned int snap      = 8; /* snap pixel */
 static const int topbar             = 1; /* 0 means bottom bar */
 static const Time zenmode           = 3; /* if set, delays showing rapid
