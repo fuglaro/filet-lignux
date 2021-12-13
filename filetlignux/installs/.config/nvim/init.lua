7de60aac807b7dc3e9f3c1579036d2b3d2e29c1a
John van Leeuwen <jvl@convex.cc>
2021-12-13 17:30:49 +0000

    nvim conf: nnn setup, shiftwidth, mouse in normal and visual

--- filetlignux/installs/.config/nvim/init.lua
+++ filetlignux/installs/.config/nvim/init.lua
@@ -1,4 +1,12 @@
 
 vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
 vim.opt.colorcolumn = "80"
+vim.opt.mouse = "nv"
 
+require('nnn').setup({
+	picker = {
+		cmd = "nnn -HG",
+		style = { width=0.5, height=0.9, xoffset=0, yoffset=1 },
+	},
+})
